812a089507ca93ff67b0dcc0478e677e83a05a1e
LeonidBolshakov <bolleoa@gmail.com>
2025-02-24 09:49:43 +0800

    Подготовлена к PyInstaller

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 class Const(frozenset):
     MESSAGE_TEXT = "Текст скопирован в буфер обмена"
-    NAME_UI = "report.ui"  # Имя файла интерфейса UI
+    NAME_UI = "_internal\\report.ui"  # Имя файла интерфейса UI
     PERCENT_CORP = 50  # Процент отчисления корпорации
     PERCENT_NDS = 5.0  # Процент НДС
     WINDOW_LIFETIME_MS = 1000  # Время отображения окна сообщения в миллисекундах
